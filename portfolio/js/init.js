48333c13d1eb04e28a76d6455621f86d657cc175
Doug Bryant <douglas.e.bryant@gmail.com>
2019-06-24 18:00:56 -0400

    Moved a js function

--- portfolio/js/init.js
+++ portfolio/js/init.js
@@ -176,6 +176,19 @@
       return false;
    });
 
+  // ========================================================================= //
+  //  Typed Js
+  // ========================================================================= //
+
+  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
 
 });
 
